8b7c83cb2a2b49cb261a7a7a3a18e45920cf0e8d
Kirill Latyshov <kirilllatyshov99@gmail.com>
2025-01-10 19:40:07 +0300

    Start price fixing

--- src/main/java/org/mpei/nti/modelCalculation/ReliabilityCalculation.java
+++ src/main/java/org/mpei/nti/modelCalculation/ReliabilityCalculation.java
@@ -11,9 +11,32 @@ public class ReliabilityCalculation {
     public static void economicDamageCalculation(List<SubstationMeasures> substationMeasuresList) {
         for (SubstationMeasures substationMeasure : substationMeasuresList) {
             if (substationMeasure.getTotalPrice() == 0.0f) {
+                int firewallCheck = 0;
+                int idsCheck = 0;
                 for (SubstationMeasuresPerYear substationMeasuresPerYear : substationMeasure.getSubstationMeasuresPerYear()) {
                     substationMeasure.setCapexPrice(substationMeasure.getCapexPrice() + substationMeasuresPerYear.getCapexPrice());
                     substationMeasure.setOpexPrice(substationMeasure.getOpexPrice() + substationMeasuresPerYear.getOpexPrice());
+
+                    if (substationMeasuresPerYear.getImprosedMeasures().getD20() == 1){
+                        firewallCheck++;
+                    } else {
+                        firewallCheck = 0;
+                    }
+                    if (substationMeasuresPerYear.getImprosedMeasures().getD21() == 1){
+                        idsCheck++;
+                    } else {
+                        idsCheck = 0;
+                    }
+
+                    if (idsCheck == 10){
+                        substationMeasure.setCapexPrice(substationMeasure.getCapexPrice() + 750000.0f);
+                        idsCheck = 0;
+                    }
+                    if (firewallCheck == 10){
+                        substationMeasure.setCapexPrice(substationMeasure.getCapexPrice() + 3250000.0f);
+                        firewallCheck = 0;
+                    }
+
                     float Mizl = OverTriggering.overTriggeringCalculation(substationMeasuresPerYear);
                     float Mlozh = FalsePositive.falsePositiveCalculation(substationMeasuresPerYear);
                     float Motk = FailureTriggering.failureTriggeringCalculation(substationMeasuresPerYear);
@@ -26,18 +49,23 @@ public class ReliabilityCalculation {
 
     public static Float capexMeasuresCalculation(SubstationMeasuresPerYear substationMeasuresPerYear) {
         float embeddedMeasuresPrice = 0.0f;
-        for (IED ied : substationMeasuresPerYear.getIedList()) {
-            embeddedMeasuresPrice += (ied.getD2() * 1083.33f + ied.getD4() * 1083.33f + ied.getD5() * 1083.33f +
-                    ied.getD8() * 1083.33f + ied.getD9() * 1083.33f + ied.getD13() * 1083.33f +
-                    ied.getD14() * 1083.33f + ied.getD15() * 1083.33f + ied.getD17() * 1083.33f +
-                    ied.getD18() * 1083.33f + ied.getD23() * 1083.33f);
+        float improsedMeasuresPrice = 0.0f;
+        if (substationMeasuresPerYear.getYearNumber() == 1) {
+            for (IED ied : substationMeasuresPerYear.getIedList()) {
+                embeddedMeasuresPrice += (ied.getD2() * 1083.33f + ied.getD4() * 1083.33f + ied.getD5() * 1083.33f +
+                        ied.getD8() * 1083.33f + ied.getD9() * 1083.33f + ied.getD13() * 1083.33f +
+                        ied.getD14() * 1083.33f + ied.getD15() * 1083.33f + ied.getD17() * 1083.33f +
+                        ied.getD18() * 1083.33f + ied.getD23() * 1083.33f);
+            }
+            improsedMeasuresPrice = substationMeasuresPerYear.getImprosedMeasures().getD3() * 20000.0f +
+                    substationMeasuresPerYear.getImprosedMeasures().getD7() * 1083.33f +
+                    substationMeasuresPerYear.getImprosedMeasures().getD19() * 1500000.0f +
+                    substationMeasuresPerYear.getImprosedMeasures().getD20() * 750000.0f +
+                    substationMeasuresPerYear.getImprosedMeasures().getD21() * 3250000.0f +
+                    substationMeasuresPerYear.getImprosedMeasures().getD24() * 150000.0f;
+        } else{
+            improsedMeasuresPrice = substationMeasuresPerYear.getImprosedMeasures().getD19() * 1500000.0f;
         }
-        float improsedMeasuresPrice = substationMeasuresPerYear.getImprosedMeasures().getD3() * 20000.0f +
-                substationMeasuresPerYear.getImprosedMeasures().getD7() * 1083.33f +
-                substationMeasuresPerYear.getImprosedMeasures().getD19() * 1500000.0f +
-                substationMeasuresPerYear.getImprosedMeasures().getD20() * 750000.0f +
-                substationMeasuresPerYear.getImprosedMeasures().getD21() * 3250000.0f +
-                substationMeasuresPerYear.getImprosedMeasures().getD24() * 150000.0f;
         return embeddedMeasuresPrice + improsedMeasuresPrice;
     }
 
